0b92f04622f6e36d709cffda67e2aaef7563dc92
Vako <98945500+vako03@users.noreply.github.com>
2023-02-24 15:09:47 +0400

    .Added Registerpage

--- Homework 24/RegisterViewController.swift
+++ Homework 24/RegisterViewController.swift
@@ -9,21 +9,38 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+   
+    @IBOutlet  private weak var usernameTextField: UITextField!
+    @IBOutlet  private weak var passwordTextField: UITextField!
+    @IBOutlet  private  weak var registerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        usernameTextField.layer.cornerRadius = 18
+        passwordTextField.layer.cornerRadius = 18
+        registerButton.layer.borderWidth = 2
+        registerButton.layer.cornerRadius = 18
+        registerButton.addTarget(self, action: #selector(registerUser), for: .touchUpInside)    }
+    @objc func registerUser() {
+        let username = usernameTextField.text!
+        let password = passwordTextField.text!
+        
+        if username.isEmpty || password.isEmpty {
+            let alert = UIAlertController(title: "Error", message: "Please fill in all fields", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        UserDefaults.standard.set(username, forKey: "username")
+        UserDefaults.standard.set(password, forKey: "password")
+        
+        let alert = UIAlertController(title: "Success", message: "User registered successfully!", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (_) in
+            self.navigationController?.popViewController(animated: true)
+        }))
+        present(alert, animated: true, completion: nil)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+
