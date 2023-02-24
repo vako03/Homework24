cbe4667fef2c617547fa66b385671deafb58d1e3
Vako <98945500+vako03@users.noreply.github.com>
2023-02-24 13:00:45 +0400

    Initial Commit

---
+++ Homework 24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 24
+//
+//  Created by valeri mekhashishvili on 2/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
